2a5357c214bac110d9995ccc6a16388c45e505da
acurber91 <acurber91@gmail.comgit config --global user.email acurber91@gmail.com>
2021-05-13 21:45:34 -0300

    Se agregan los casos de prueba correspondiente a la carga de un valor a una lista vacía y a la recuperación del valor a partir de la clave.

--- src/lista.js
+++ src/lista.js
@@ -1,13 +1,33 @@
 module.exports = class Lista {
+    
+    #elementos;
+    
     constructor() {
-
+        this.#elementos = [];
     }
     
     count() {
-        return 0;
+        return this.#elementos.length;
     }
 
-    find() {
-        return NaN;
+    find(key) {
+        var value; 
+        for(var i = 0; i < this.#elementos.length; i++) {
+            if(this.#elementos[i].key == key) {
+                value = this.#elementos[i].value;
+            }
+        }
+        if(value)
+        {
+            return value;
+        }
+        else
+        {
+            return NaN;
+        }
     }
-}
+
+    add(key, value) {
+        this.#elementos.push({key, value});
+    }
+};
--- test/test_lista.js
+++ test/test_lista.js
@@ -1,6 +1,4 @@
 /*
- * Cuando se agrega un elemento a una lista vacía hay un elemento.
- * Cuando se agrega un elemento a una lista vacía se puede recuperar el valor a partir de la clave.
  * Cuando se agrega una clave que ya está en la lista se actualiza el valor correspondiente.
  * Cuando se agregan un elemento a una lista vacía la lista de claves esta ordenada.
  * Cuando se agrega un elemento al principio la lista de claves esta ordenada.
@@ -10,13 +8,23 @@
 const assert = require("chai").assert;
 const Lista = require("../src/lista.js");
 
-describe("En una lista vacia", function() {
+describe("En una lista vacia:", function() {
     var lista = new Lista();
-    it("Hay cero elementos", function() {
+    it("Hay cero elementos.", function() {
         assert.equal(lista.count(), 0);
     })
-
-    it("No se encuentra ninguna clave", function () {
+    it("No se encuentra ninguna clave.", function () {
         assert.isNaN(lista.find("clave"));
+    })
+})
+
+describe("Cuando se crea una lista con un elemento:", function() {
+    var lista = new Lista();
+    lista.add("clave", "valor");
+    it("Hay un elemento.", function() {
+        assert.equal(lista.count(), 1);
+    })
+    it("Se recupera el valor a partir de la clave.", function() {
+        assert.equal(lista.find("clave"), "valor");
     })
 })
